eacbac98961bfb3a6c205fafbb2570dc4c924021
Imuzeze <128602719+Imuzeze@users.noreply.github.com>
2024-04-25 13:51:08 +0100

    Update chatbotdata.py

--- chatbotdata.py
+++ chatbotdata.py
@@ -10,9 +10,9 @@ import streamlit as st
 lemmatizer = nltk.stem.WordNetLemmatizer()
 
 #Download required NLTK data 
-# nltk.download('stopwords')
-# nltk.download('punkt')
-# nltk.download('wordnet')
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('wordnet')
 
 
 data = pd.read_csv('chatbot dataset.txt', sep = "\t", header = None)
